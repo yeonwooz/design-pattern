946fa34fabe2bfd2059bf3d3d82ad1b05dac2941
Suyeon Woo <woosean999@gmail.com>
2024-12-01 14:01:17 +0900

    feat: to packages-observerPattern

---
+++ design-pattern-project/src/observerPattern/StatisticsDisplay.java
@@ -0,0 +1,13 @@
+package observerPattern;
+
+public class StatisticsDisplay implements Observer, DisplayElement{
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void display() {
+
+    }
+}
---
+++ design-pattern-project/src/observerPattern/WeatherData.java
@@ -0,0 +1,54 @@
+package observerPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject {
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<Observer>();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer: observers) {
+            observer.update();
+        }
+    }
+
+    public float getTemperature(){
+        return this.temperature;
+    }
+    public float getHumidity(){
+        return this.humidity;
+
+    }
+    public float getPressure(){
+        return this.pressure;
+
+    }
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+}
